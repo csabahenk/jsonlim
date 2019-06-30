5c606ed367927e837d3f13fee3fc4e91d7556cdf
Csaba Henk <csaba@lowlife.hu>
2019-06-30 12:28:48 +0200

    refactor code under JSONlim module
    JSONlim.format does not expose internal state arguments
    threaded through recursuon.

--- jsonlim.rb
+++ jsonlim.rb
@@ -2,50 +2,60 @@
 
 require 'json'
 
-def jsonlim(obj, max_depth=-1, out: STDOUT, indent: " "*4, depth: 0, prefix: "", postfix: "")
-  height = proc { |o|
-    case o
-    when [],{}
-      0
-    when Hash
-      o.values.map(&height).max + 1
-    when Array
-      o.map(&height).max + 1
-    else
-      0
+module JSONlim
+extend self
+
+  private def format_rec(obj, max_depth=-1, out: STDOUT, indent: " "*4,
+                         depth: 0, prefix: "", postfix: "")
+    height = proc { |o|
+      case o
+      when [],{}
+        0
+      when Hash
+        o.values.map(&height).max + 1
+      when Array
+        o.map(&height).max + 1
+      else
+        0
+      end
+    }
+
+    if max_depth and max_depth < 0
+      max_depth = [height[obj]+max_depth, 0].max
     end
-  }
 
-  if max_depth and max_depth < 0
-    max_depth = [height[obj]+max_depth, 0].max
+    if (max_depth||depth+1) <= depth or !(Hash === obj or Array === obj) or obj.empty?
+      return out << indent*depth + prefix + obj.to_json + postfix + "\n"
+    end
+
+    delim_open, delim_close = case obj
+    when Hash
+      %w[{ }]
+    when Array
+      %w<[ ]>
+    end
+
+    out << indent*depth + prefix + delim_open + "\n"
+    case obj
+    when Hash
+      obj.each_with_index { |w,i|
+        k,v=w
+        format_rec(v, max_depth, out: out, indent: indent, depth: depth+1,
+                prefix: k.to_s.to_json + ": ", postfix: (i==obj.size-1) ? "" : ?,)
+      }
+    when Array
+      obj.each_with_index { |v,i|
+        format_rec(v, max_depth, out: out, indent: indent, depth: depth+1,
+                postfix: (i==obj.size-1) ? "" : ?,)
+      }
+    end
+    out << indent*depth + delim_close + postfix + "\n"
   end
 
-  if (max_depth||depth+1) <= depth or !(Hash === obj or Array === obj) or obj.empty?
-    return out << indent*depth + prefix + obj.to_json + postfix + "\n"
+  def format obj, max_depth=-1, out: STDOUT, indent: " "*4
+    format_rec obj, max_depth, out: out, indent: indent
   end
 
-  delim_open, delim_close = case obj
-  when Hash
-    %w[{ }]
-  when Array
-    %w<[ ]>
-  end
-
-  out << indent*depth + prefix + delim_open + "\n"
-  case obj
-  when Hash
-    obj.each_with_index { |w,i|
-      k,v=w
-      jsonlim(v, max_depth, out: out, indent: indent, depth: depth+1,
-              prefix: k.to_s.to_json + ": ", postfix: (i==obj.size-1) ? "" : ?,)
-    }
-  when Array
-    obj.each_with_index { |v,i|
-      jsonlim(v, max_depth, out: out, indent: indent, depth: depth+1,
-              postfix: (i==obj.size-1) ? "" : ?,)
-    }
-  end
-  out << indent*depth + delim_close + postfix + "\n"
 end
 
 if __FILE__ == $0
@@ -61,5 +71,5 @@ if __FILE__ == $0
     op.on("-f", "--input-format=F", formats.keys.join(?,)) { |f| format = f }
   end.parse!
 
-  jsonlim formats[format].load($<), *[lvl].compact
+  JSONlim.format formats[format].load($<), *[lvl].compact
 end
