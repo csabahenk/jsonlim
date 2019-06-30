a6138ab1db7600914a2c565125d667a62695f8f9
Csaba Henk <csaba@lowlife.hu>
2019-06-30 11:39:17 +0200

    change terminology: s/depth/height/; s/level/depth/
    Height is an inherent charactersitic of the tree,
    depth makes sense in context of a tree and one of it's
    element.
    
    Depth was used so far as a shorthand for "max depth occurring
    in tree" but that's nothing else then height proper.

--- jsonlim.rb
+++ jsonlim.rb
@@ -2,26 +2,26 @@
 
 require 'json'
 
-def jsonlim(obj, max_level=-1, out: STDOUT, indent: " "*4, level: 0, prefix: "", postfix: "")
-  depth = proc { |o|
+def jsonlim(obj, max_depth=-1, out: STDOUT, indent: " "*4, depth: 0, prefix: "", postfix: "")
+  height = proc { |o|
     case o
     when [],{}
       0
     when Hash
-      o.values.map(&depth).max + 1
+      o.values.map(&height).max + 1
     when Array
-      o.map(&depth).max + 1
+      o.map(&height).max + 1
     else
       0
     end
   }
 
-  if max_level and max_level < 0
-    max_level = [depth[obj]+max_level, 0].max
+  if max_depth and max_depth < 0
+    max_depth = [height[obj]+max_depth, 0].max
   end
 
-  if (max_level||level+1) <= level or !(Hash === obj or Array === obj) or obj.empty?
-    return out << indent*level + prefix + obj.to_json + postfix + "\n"
+  if (max_depth||depth+1) <= depth or !(Hash === obj or Array === obj) or obj.empty?
+    return out << indent*depth + prefix + obj.to_json + postfix + "\n"
   end
 
   delim_open, delim_close = case obj
@@ -31,21 +31,21 @@ def jsonlim(obj, max_level=-1, out: STDOUT, indent: " "*4, level: 0, prefix: "",
     %w<[ ]>
   end
 
-  out << indent*level + prefix + delim_open + "\n"
+  out << indent*depth + prefix + delim_open + "\n"
   case obj
   when Hash
     obj.each_with_index { |w,i|
       k,v=w
-      jsonlim(v, max_level, out: out, indent: indent, level: level+1,
+      jsonlim(v, max_depth, out: out, indent: indent, depth: depth+1,
               prefix: k.to_s.to_json + ": ", postfix: (i==obj.size-1) ? "" : ?,)
     }
   when Array
     obj.each_with_index { |v,i|
-      jsonlim(v, max_level, out: out, indent: indent, level: level+1,
+      jsonlim(v, max_depth, out: out, indent: indent, depth: depth+1,
               postfix: (i==obj.size-1) ? "" : ?,)
     }
   end
-  out << indent*level + delim_close + postfix + "\n"
+  out << indent*depth + delim_close + postfix + "\n"
 end
 
 if __FILE__ == $0
@@ -57,7 +57,7 @@ if __FILE__ == $0
   format = 'json'
 
   OptionParser.new do |op|
-    op.on("-l", "--level=N", Integer, "level up to which unfold") { |n| lvl = n }
+    op.on("-d", "--depth=N", Integer, "depth up to which unfold") { |n| lvl = n }
     op.on("-f", "--input-format=F", formats.keys.join(?,)) { |f| format = f }
   end.parse!
 
